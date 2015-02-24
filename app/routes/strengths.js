2558cf0df218eee7bb1e54c0719f51068aba2ead
troyswanson <gerphimum@gmail.com>
2015-02-23 19:01:08 -0600

    Add the strengths/typeahead route
    Also changes the single strength GET to singular (/strength/:title)

--- app/routes/strengths.js
+++ app/routes/strengths.js
@@ -1,10 +1,16 @@
 /*jslint node: true*/
 "use strict";
 
-var Strength = require('../model/strength.js').strength;
+var Strength = require('../model/strength').strength;
 
 exports.view = function (req, res) {
   Strength.findOne({title: req.params.title}, '-_id', function (err, s) {
+    res.json(s);
+  });
+};
+
+exports.typeahead = function (req, res) {
+  Strength.find({title: {$regex: '^' + req.params.q, $options: 'i'}}, 'title -_id', function (err, s) {
     res.json(s);
   });
 };
--- index.js
+++ index.js
@@ -6,9 +6,10 @@ var express = require('express');
 var app = express();
 var router = express.Router();
 
-var strengths = require('./app/routes/strengths.js');
+var strengths = require('./app/routes/strengths');
 
-router.route('/strengths/:title').get(strengths.view);
+router.route('/strength/:title').get(strengths.view);
+router.route('/strengths/typeahead/:q').get(strengths.typeahead);
 router.route('/strengths/search/:q').get(strengths.search);
 
 app.use(express.static(__dirname + '/public'));
